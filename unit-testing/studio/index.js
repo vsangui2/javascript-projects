779d9d9f17e7dfd7e43dcd2fde944dcc6323beb9
Vincent Sanguinetti <vincent.m.sanguinetti@gmail.com>
2024-02-08 20:21:24 -0600

    Unit Test Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,25 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(number){
+    if (number %2 === 0 && number %3 === 0){
+            return "LaunchCode!"
+    } else if (number %2 === 0 && number %5 === 0){
+        return "Launch Rocks!"
+    } else if (number %2 === 0){
+        return "Launch!"
+    } else if (number %3 === 0 && number %5 === 0){
+            return "Code Rocks!"
+    } else if (number %3 === 0){
+        return "Code!"
+    } else if (number %5 === 0){
+        return "Rocks!"
+    }
 }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,35 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("organizational value", function(){
+    expect(launchcode.organization).toBe("nonprofit");
+  });
 
+  test("executive name", function(){
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("percentage of employees", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+
+  test("Programs offered", function(){
+    expect(launchcode.programsOffered[0]).toEqual("Web Development", true);
+    expect(launchcode.programsOffered[1]).toEqual("Data Analysis", true);
+    expect(launchcode.programsOffered[2]).toEqual("Liftoff", true);
+    expect(launchcode.programsOffered.length).toEqual(3, true);
+  });
+
+test("Method check", function(){
+  expect(launchcode.launchOutput(2)).toBe("Launch!");
+  expect(launchcode.launchOutput(3)).toBe("Code!");
+  expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  expect(launchcode.launchOutput(10)).toBe("Launch Rocks!")
+});
   // Write your unit tests here!
+
+
   
 });
